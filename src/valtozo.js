a01ee1a056c3f9b5c8a41deab68cca4abf7a638b
vtg <varastothgergo@ktch.hu>
2024-03-12 10:02:57 +0100

    let

--- src/valtozo.js
+++ src/valtozo.js
@@ -2,3 +2,14 @@
 const doboz = document.getElementById("doboz");
  
 doboz.innerHTML = "VTG"
+//doboz.textContent = "kiskutya"
+
+var a = 3
+var a = 7 
+{
+    let a = 69
+    console.log(a)
+}
+console.log(a)
+
+
